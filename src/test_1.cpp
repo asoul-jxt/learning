6db79ec2bd23ea1fbab254e378128ed7b810132d
userDiana <ll5n@qq.com>
2023-12-14 03:27:28 +0800

    version 1.0

---
+++ src/test_1.cpp
@@ -0,0 +1,5 @@
+//
+// Created by 18398 on 2023/12/14.
+//
+
+#include "test_1.h"
---
+++ src/test_1.h
@@ -0,0 +1,14 @@
+//
+// Created by 18398 on 2023/12/14.
+//
+
+#ifndef LEARNING_TEST_1_H
+#define LEARNING_TEST_1_H
+
+
+class test_1 {
+
+};
+
+
+#endif //LEARNING_TEST_1_H
